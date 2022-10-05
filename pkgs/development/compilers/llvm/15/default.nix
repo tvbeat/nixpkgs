9ff04d821eb729929ef97f4e7e321d92c5fd5b79
Rahul Butani <rrbutani@users.noreply.github.com>
2022-10-05 10:16:21 -0500

    llvmPackages_15: updates for LLVM 15
    None of the patches required any touch-up; the only change of note is:
      - due to changes in the libc++/libc++abi build
        (https://reviews.llvm.org/D120719 and https://reviews.llvm.org/D131037)
        we have to add an extra build option to the libc++ header only
        build that sidesteps bits of the libc++ build config that assume
        libc++-abi is present in the build:
        https://github.com/llvm/llvm-project/blob/4f827318e3e8ccab4ff131e06234caa827e91e4e/libcxx/src/CMakeLists.txt#L255-L256
    
    Rather than maintaining a precise set of build options that let us dodge
    referencing libc++-abi variables in the libc++ header only build, we set
    `LIBCXX_CXX_ABI` to `none`, as suggested by @lovesegfault.
    
    More discussion about this here: https://github.com/NixOS/nixpkgs/pull/194634#discussion_r990267037
    
    Co-authored-by: Bernardo Meurer <bernardo@meurer.org>

--- pkgs/development/compilers/llvm/15/default.nix
+++ pkgs/development/compilers/llvm/15/default.nix
@@ -18,7 +18,7 @@
 }:
 
 let
-  release_version = "14.0.6";
+  release_version = "15.0.3";
   candidate = ""; # empty or "rcN"
   dash-candidate = lib.optionalString (candidate != "") "-${candidate}";
   rev = ""; # When using a Git commit
@@ -30,7 +30,7 @@ let
     owner = "llvm";
     repo = "llvm-project";
     rev = if rev != "" then rev else "llvmorg-${version}";
-    sha256 = "sha256-vffu4HilvYwtzwgq+NlS26m65DGbp6OSSne2aje1yJE=";
+    sha256 = "sha256-/lQoiSZtiJlGllkTAokjzEMCvBXmmWR41XPBk1+Oq68=";
   };
 
   llvm_meta = {
@@ -87,7 +87,7 @@ let
       python3 = pkgs.python3;  # don't use python-boot
     });
 
-    # TODO: lldb/docs/index.rst:155:toctree contains reference to nonexisting document 'design/structureddataplugins'
+    # Needs package for spinhx-automodapi: https://github.com/astropy/sphinx-automodapi
     # lldb-manpages = lowPrio (tools.lldb.override {
     #   enableManpages = true;
     #   python3 = pkgs.python3;  # don't use python-boot
