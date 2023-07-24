5780fc40da7b165365cf12bb438aee510bff7c47
Luka Blaskovic <lblasc@tvbeat.com>
2023-07-24 14:13:33 +0000

    rustc: remove --tools=rustc, to generate rust-analyzer-proc-macro-srv

--- pkgs/development/compilers/rust/rustc.nix
+++ pkgs/development/compilers/rust/rustc.nix
@@ -75,7 +75,6 @@ in stdenv.mkDerivation rec {
     "--release-channel=stable"
     "--set=build.rustc=${rustc}/bin/rustc"
     "--set=build.cargo=${cargo}/bin/cargo"
-    "--tools=rustc"
     "--enable-rpath"
     "--enable-vendor"
     "--build=${rust.toRustTargetSpec stdenv.buildPlatform}"
