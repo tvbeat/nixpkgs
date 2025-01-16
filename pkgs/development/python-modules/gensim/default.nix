db443cc809f36ddf310a97e51d43634ff843d0e8
Luka Blašković <lblasc@tvbeat.com>
2025-01-16 07:17:15 +0000

    python3Packages.gensim: switch to numpy2 PR, relax numpy dep

--- pkgs/development/python-modules/gensim/default.nix
+++ pkgs/development/python-modules/gensim/default.nix
@@ -2,11 +2,10 @@
   lib,
   buildPythonPackage,
   cython_0,
-  oldest-supported-numpy,
-  setuptools,
-  fetchPypi,
-  mock,
   numpy,
+  setuptools,
+  fetchFromGitHub,
+  mock,
   scipy,
   smart-open,
   pyemd,
@@ -22,16 +21,24 @@ buildPythonPackage rec {
   # C code generated with CPython3.12 does not work cython_0.
   disabled = !(pythonOlder "3.12");
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-hIUgdqaj2I19rFviReJMIcO4GbVl4UwbYfo+Xudtz1c=";
+  src = fetchFromGitHub {
+    owner = "julianpollmann";
+    repo = "gensim";
+    rev = "122f4ae80ccf7b27d49889ba9ac4f338ed082b7b";
+    hash = "sha256-OFm5B8htSYNvevcfaWFtbM+nJZwEnEkFJlPn5FzEvPA=";
   };
 
   build-system = [
     cython_0
-    oldest-supported-numpy
+    numpy
     setuptools
   ];
+
+  postPatch = ''
+    substituteInPlace pyproject.toml \
+      --replace-fail "Cython>=0.29.32,<3.0.0" "Cython" \
+      --replace-fail "numpy==2.0.0; python_version>='3.9' and platform_machine not in 'arm64|aarch64'" "numpy"
+  '';
 
   dependencies = [
     smart-open
@@ -47,6 +54,7 @@ buildPythonPackage rec {
 
   pythonRelaxDeps = [
     "scipy"
+    "numpy"
   ];
 
   pythonImportsCheck = [ "gensim" ];
