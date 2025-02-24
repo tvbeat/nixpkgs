1e46f5a8c7f124c103416762405dc103cea31e14
Bruce Toll <4109762+tollb@users.noreply.github.com>
2025-02-24 16:34:25 -0500

    nixos/tests/systemd-journal: Check container for systemd-journald-audit
    Containers should not have a systemd-journal-audit.socket, as there is
    no audit support in containers.
    
    Backport: https://github.com/NixOS/nixpkgs/pull/384924

--- nixos/tests/systemd-journal.nix
+++ nixos/tests/systemd-journal.nix
@@ -18,6 +18,12 @@ import ./make-test-python.nix (
       services.journald.audit = true;
       environment.systemPackages = [ pkgs.audit ];
     };
+    nodes.containerCheck = {
+      containers.c1 = {
+        autoStart = true;
+        config = { };
+      };
+    };
 
     testScript = ''
       machine.wait_for_unit("multi-user.target")
@@ -50,6 +56,16 @@ import ./make-test-python.nix (
         # logs ideally should NOT end up in kmesg, but they do due to
         # https://github.com/systemd/systemd/issues/15324
         journaldAudit.succeed("journalctl _TRANSPORT=kernel --grep 'unit=systemd-journald'")
+
+      with subtest("container systemd-journald-audit disabled"):
+        containerCheck.wait_for_unit("multi-user.target");
+        containerCheck.wait_until_succeeds("systemctl -M c1 is-active default.target");
+
+        # systemd-journald-audit.socket should not exist, so we expect exit status 4: "no such unit"
+        (status, output) = containerCheck.execute("systemctl -M c1 status systemd-journald-audit.socket")
+        containerCheck.log(output)
+        assert status == 4, f"systemd-journald-audit.socket should not exist in a container so we expect an exit status of 4, but status was: {status}"
+
     '';
   }
 )
