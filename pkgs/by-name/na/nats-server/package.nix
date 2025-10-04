9aef217aa075562016bc7c62cf31cd08816de3a6
Luka Blašković <lblasc@tvbeat.com>
2025-10-04 06:35:43 +0000

    nats-server: 2.11.9 -> 2.11.10

--- pkgs/by-name/na/nats-server/package.nix
+++ pkgs/by-name/na/nats-server/package.nix
@@ -7,16 +7,16 @@
 
 buildGoModule rec {
   pname = "nats-server";
-  version = "2.11.9";
+  version = "2.11.10";
 
   src = fetchFromGitHub {
     owner = "nats-io";
     repo = "nats-server";
     rev = "v${version}";
-    hash = "sha256-F35qklJRa9IUikVNhh8c34KF3l41yfPXl0aig1iQq1g=";
+    hash = "sha256-xTum+LimUidz8mnAG3C2JwoYOFJCdReKPyHGyldEkME=";
   };
 
-  vendorHash = "sha256-ti2Y4ev8INMNudfwmPhmKGCManRj2cFTw6Fh7kkvDQc=";
+  vendorHash = "sha256-wK+J5Km7k33nLeafWTisx2WSfShX5sNyFB377EEsbXM=";
 
   doCheck = false;
 
